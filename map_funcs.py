f2adef327e8d458c83b5616362233aed434bcfff
gangsterjens <90387893+gangsterjens@users.noreply.github.com>
2023-11-30 10:04:40 +0100

    Create map_funcs.py

---
+++ map_funcs.py
@@ -0,0 +1,7 @@
+def folium_map(latitude, longitude, width=725):
+    m = folium.Map(location=[latitude, longitude], zoom_start=16)
+    folium.Marker(
+        [latitude, longitude]
+    ).add_to(m)
+
+    return st_folium(m, width=width, returned_objects=[])
