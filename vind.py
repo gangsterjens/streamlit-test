68e09e41c3d72362a91d4cdfac7433fb9d0af5ce
gangsterjens <90387893+gangsterjens@users.noreply.github.com>
2023-11-30 10:08:50 +0100

    Update vind.py

--- vind.py
+++ vind.py
@@ -6,6 +6,7 @@ import ast
 import streamlit as st
 import datetime
 import weather_funcs as wf
+import map_funcs as mf
 import folium
 from streamlit_folium import st_folium
 
@@ -34,7 +35,8 @@ if kjoyr and (type_naturskade == 'Storm'):
 elif kjoyr and (type_naturskade == 'Flom'):
     st.markdown("Skadedata fra flom")
     gdf_list = wf.find_water(float(longitude), float(latitude), distance=1000)
-    st.markdown(gdf_list)
+    st.markdown(f"Nærmeste vann er {gdf_list[7]} km unna" )
     map_df = pd.DataFrame(gdf_list[4:6], columns=['lon', 'lat'])
+    mf.folium_map(float(latitude), float(longitude))
     st.map(map_df, zoom=15, size=3)
 
