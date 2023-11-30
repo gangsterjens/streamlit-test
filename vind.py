81c4d60dcdbae4e4e83d3c91fb3179890b030b2f
gangsterjens <90387893+gangsterjens@users.noreply.github.com>
2023-11-30 10:04:12 +0100

    Update vind.py

--- vind.py
+++ vind.py
@@ -10,18 +10,6 @@ import folium
 from streamlit_folium import st_folium
 
 
-########################################################################################################################
-# center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-).add_to(m)
-
-# call to render Folium map in Streamlit, but don't get any data back
-# from the map (so that it won't rerun the app when the user interacts)
-st_folium(m, width=725, returned_objects=[])
-########################################################################################################################
-
 client_id = '9d4a669c-ad20-4ebe-9e0c-8c82b39a22ec'
 
 st.markdown("# Naturskadedata")
