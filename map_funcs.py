f192032564f374684bc32019142f7c1d39f4cc3c
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-06-12 13:56:40 +0200

    Update map_funcs.py

--- map_funcs.py
+++ map_funcs.py
@@ -4,7 +4,10 @@ from streamlit_folium import st_folium
 def folium_map(latitude, longitude, width=725):
     m = folium.Map(location=[latitude, longitude], zoom_start=16)
     folium.Marker(
-        [latitude, longitude], popup="147", tooltip="147 moh"
+        [latitude, longitude], popup="hus", tooltip="Hus 7.8 moh"
+    ).add_to(m)
+    folium.Marker(
+        [63.27373, 9.83214], popup="vann", tooltip="Vann 7.0 moh"
     ).add_to(m)
 
     return st_folium(m, width=width, returned_objects=[])
